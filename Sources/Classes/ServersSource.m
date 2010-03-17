320311bfaa6d392bfe49804fea6481e0c8d56890
Stanislav Yudin <Stanly@Silverblaze.local>
2010-03-17 17:12:38 +0530

    more logging, report access, handle NSException

--- Sources/Classes/ServersSource.m
+++ Sources/Classes/ServersSource.m
@@ -30,7 +30,10 @@
 	}
 	else {
 		//displaying servers
-		return [ [[[NSApp delegate] serversManager] getServers] objectAtIndex:index];
+		id servers = [[[NSApp delegate] serversManager] getServers];
+		id srv = [servers objectAtIndex:index];
+		NSLog(@"Displaying server at index %d", index);
+		return srv;
 	}
 }
 
@@ -38,89 +41,102 @@
    isItemExpandable:(id)item
 {
 	//server class is expandable
-	if ([item entity] == [HyperTableServer entityDescription])
+	if (item && [item entity] == [HyperTableServer entityDescription]) {
+		NSLog(@"Server is expandable");
 		return YES;
-	
-	return NO;
+	}
+	else {
+		NSLog(@"Table is not expandable");
+		return NO;
+	}
 }
 
 - (NSInteger)outlineView:(NSOutlineView *)outlineView
   numberOfChildrenOfItem:(id)item
 {
-	if (item) {
-		//return number of tables in server item
-		int tablesCount = 0;
-		@try {
+	NSLog(@"Reading number of servers/tables");
+	@try {
+		//request for item
+		if (item) {
+			NSLog(@"Getting tables of server %s", [[item valueForKey:@"hostname"] UTF8String]);
+			//return number of tables in server item
+			int tablesCount = 0;
 			tablesCount = [[item valueForKey:@"tables"] count];
-		}
-		@catch(NSException * ex) {
-			NSLog(@"Failed to query tables from datastore");
-		}
-		
-		
-		if (tablesCount <= 0) {
-			NSLog(@"Updating tables list");
-			
-			//get tables
-			DataRow * row = row_new("Tables");
-			id connection = [ [[NSApp delegate] serversManager] getConnectionForServer:item];
-			if (!connection || ![connection isConnected]) {
-				NSLog(@"Error: Connection is NOT READY for tables list update");
-				[[[NSApp delegate] serversManager] reconnectServer:item];
-				return 0;
-			}
-			
-			//read tables
-			get_tables_list([connection thriftClient], row);
-			DataCellIterator * ci = cell_iter_new(row);
-			DataCell * cell = NULL;
-			do {
-				cell = cell_iter_next_cell(ci);
-				if (cell) {
-					HyperTable * newTable = [HyperTable tableWithDefaultContext];
-					[newTable setValue:[NSString stringWithCString:cell->cellValue 
-														  encoding:NSUTF8StringEncoding] 
-								forKey:@"name"];
-					[newTable setValue:item forKey:@"server"];
-					[[[NSApp delegate] managedObjectContext] insertObject:newTable];
+			if (tablesCount <= 0) {
+				NSLog(@"Requesting tables list");
+				
+				//get tables
+				DataRow * row = row_new("Tables");
+				id connection = [ [[NSApp delegate] serversManager] getConnectionForServer:item];
+				if (!connection || ![connection isConnected]) {
+					NSLog(@"Error: Connection is NOT READY for tables list update");
+					[[[NSApp delegate] serversManager] reconnectServer:item];
+					return 0;
 				}
-			} while (cell);
-			free(ci);
-			
-			NSLog(@"Saving tables list");
-			NSError * error = nil;
-			[[[NSApp delegate] managedObjectContext] save:&error];
-			if (error) {
-				[[NSApp delegate] setMessage:@"Failed to save tables to persistent store"];
-				return 0;
+				
+				//read tables
+				get_tables_list([connection thriftClient], row);
+				DataCellIterator * ci = cell_iter_new(row);
+				DataCell * cell = NULL;
+				do {
+					cell = cell_iter_next_cell(ci);
+					if (cell) {
+						HyperTable * newTable = [HyperTable tableWithDefaultContext];
+						[newTable setValue:[NSString stringWithCString:cell->cellValue 
+															  encoding:NSUTF8StringEncoding] 
+									forKey:@"name"];
+						[newTable setValue:item forKey:@"server"];
+						[[[NSApp delegate] managedObjectContext] insertObject:newTable];
+					}
+				} while (cell);
+				free(ci);
+				
+				NSLog(@"Saving tables list");
+				NSError * error = nil;
+				[[[NSApp delegate] managedObjectContext] save:&error];
+				if (error) {
+					[[NSApp delegate] setMessage:@"Failed to save tables to persistent store"];
+					return 0;
+				}
+				//get number of added tables
+				tablesCount = [[item valueForKey:@"tables"] count];
 			}
-			//get number of added tables
-			tablesCount = [[item valueForKey:@"tables"] count];
+			NSLog(@"%d tables found on server %s", tablesCount, [[item valueForKey:@"hostname"] UTF8String]);
+			return tablesCount;
 		}
-		
-		NSLog(@"%d tables found on server %s", tablesCount, [[item valueForKey:@"hostname"] UTF8String]);
-		return tablesCount;
+		//request for list of servers
+		int serversCount = [ [ [[NSApp delegate] serversManager] getServers] count];
+		NSLog(@"Servers in datastore %d", serversCount);
+		//return number of servers
+		return serversCount;
 	}
-	int serversCount = [ [ [[NSApp delegate] serversManager] getServers] count];
-	NSLog(@"Servers in datastore %d", serversCount);
-	//return number of servers
-	return serversCount;
+	@catch(NSException * ex) {
+		NSLog(@"Failed to query tables from datastore");
+		return 0;
+	}
 }
 
 - (id)outlineView:(NSOutlineView *)outlineView
 objectValueForTableColumn:(NSTableColumn *)tableColumn
 		   byItem:(id)item
 {
+	NSLog(@"Getting object value");
 	if (item) {
 		//return hostname of server or name of table
-		if ([item entity] == [HyperTableServer entityDescription])
+		if ([item entity] == [HyperTableServer entityDescription]) {
+			NSLog(@"Server object value");
+			NSLog(@"%s", [[item valueForKey:@"hostname"] UTF8String]);
 			return [item valueForKey:@"hostname"];
-		else if ([item entity] == [HyperTable entityDescription])
+		}
+		else if ([item entity] == [HyperTable entityDescription]) {
+			NSLog(@"Table object value");
+			NSLog(@"%s", [[item valueForKey:@"name"] UTF8String]);
 			return [item valueForKey:@"name"];
+		}
 		else
 			return @"Unknown";
 	}
-	return nil;
+	return @"Nothing";
 }
 
 @end
