5a7925b683d8326883493ee6c013d41e8afbe7f8
Stanislav Yudin <Stanly@Silverblaze.local>
2010-07-31 20:38:47 +0530

    implement autoreconnect servers, fix number of memory problems

--- Sources/Classes/ServersManager.m
+++ Sources/Classes/ServersManager.m
@@ -11,13 +11,97 @@
 
 @implementation ServersManager
 
+- (void)dealloc
+{
+	[connectionsCache release];
+	[super dealloc];
+}
+
+- (id)init
+{
+	[super init];
+	NSLog(@"Initializing connections cache");
+	connectionsCache = [[NSMutableDictionary alloc] init];
+	return self;
+}
+
 - (void)reconnectServer:(NSManagedObject *)server {
-	NSLog(@"Reconnecting server \"%s\"\n", [[server valueForKey:@"ipAddress"] UTF8String]);
+	[server retain];
+	NSString * address = [server valueForKey:@"ipAddress"];
+	int port  = [[server valueForKey:@"port"] intValue];
 	
-	//show connection dialog
-	[[[NSApp delegate] connectionSheetController] showSheet:self 
-							 toHost:[server valueForKey:@"ipAddress"] 
-							andPort:38080];
+	id generalPrefs = [[NSApp delegate] getSettingsByName:@"GeneralPrefs"];
+	if (!generalPrefs) {
+		[[NSApp delegate] setMessage:@"Failed to read general preferences from storage. Please recreate HyperTableBrowser.xml"];
+		return;
+	}
+	int autoReconnectServer =  [[generalPrefs valueForKey:@"autoReconnectServer"] intValue];
+	[generalPrefs release];
+	if ( autoReconnectServer ) {
+		//reconnect server with saved values
+		NSLog(@"Autoreconnecting server \"%s:%d\"\n", [address UTF8String], port);
+		[[NSApp delegate] indicateBusy];
+		[[NSApp delegate] setMessage:[NSString stringWithFormat:@"Reconnecting to server %s...",
+									  [address	UTF8String]]];
+		
+		ThriftConnectionInfo * serverInfo = [ThriftConnectionInfo infoWithAddress:address
+																		  andPort:port];
+		ConnectOperation * connectOp = [ConnectOperation connectWithInfo:serverInfo];
+		[connectOp setCompletionBlock: ^ {
+			[[NSApp delegate] indicateDone];
+			NSLog(@"Automatic reconnect: Operation complete.\n");
+			if ( ![[connectOp connection] isConnected] ) {
+				[[NSApp delegate] indicateDone];
+				NSLog(@"Automatic reconnect: Connection failed!\n");
+				[[NSApp delegate] setMessage: @"Failed to automatically reconnect to server."];
+				
+				//maybe user wants to connect with other port or address
+				//show connection sheet for that
+				[[[NSApp delegate] connectionSheetController] showSheet:self
+																 toHost:address
+																andPort:port];
+			}
+			else {
+				NSLog(@"Automatic reconnect: Connection successful!");
+				
+				[[[NSApp delegate] window] setTitle:[NSString stringWithFormat:@"HyperTable Browser @ %s", [address UTF8String]] ];
+				[[NSApp delegate] setMessage: [NSString stringWithFormat:@"Connected to %s.", 
+											   [address UTF8String]]];
+				
+				//set connection
+				[[[NSApp delegate] serversManager] setConnection:[connectOp connection] forServer:server];
+				
+				//fetch tables for new connection
+				FetchTablesOperation * fetchTablesOp = [FetchTablesOperation fetchTablesFromConnection:[connectOp connection]];
+				[fetchTablesOp setCompletionBlock: ^ {
+					NSLog(@"Automatic reconnect: Updaing servers & tables tree...\n");
+					[[[NSApp delegate] serversView] reloadItem:nil reloadChildren:YES];
+					int serverIndex = [[[NSApp delegate] serversView] rowForItem:server];
+					[[[NSApp delegate] serversView] selectRowIndexes:[NSIndexSet indexSetWithIndex:serverIndex]
+												byExtendingSelection:NO];
+					[[[NSApp delegate] toolBarController] setAllowNewTable:1];
+					[[[[NSApp delegate] toolBarController] toolBar] validateVisibleItems];
+				}];
+				
+				NSLog(@"Automatic reconnect: Refreshing tables...\n");
+				//start fetching tables
+				[[[NSApp delegate] operations] addOperation: fetchTablesOp];
+				[fetchTablesOp release];
+			}
+		} ];
+		
+		//add operation to queue
+		[[[NSApp delegate] operations] addOperation: connectOp];
+		[connectOp release];
+	}
+	else {
+		NSLog(@"Reconnecting server \"%s\"\n", [address UTF8String]);
+		//show connection dialog
+		[[[NSApp delegate] connectionSheetController] showSheet:self
+														 toHost:address
+														andPort:port];
+	}
+	[server release];
 }
 
 - (NSArray *)getServers {
@@ -37,18 +121,21 @@
 	[err release];
 	[r release];
 	NSLog(@"There are %d servers stored.\n", [serversArray count]);
-	return serversArray;
+	return [serversArray retain];
 }
 
 - (HyperTableServer *)getServer:(NSString *)ipAddress {
 	NSLog(@"Looking for server \"%s\"", [ipAddress UTF8String]);
-	for (id srv in [self getServers]) {
+	NSArray * servers = [self getServers];
+	for (id srv in servers) {
 		NSLog(@"Checking %s", [[srv valueForKey:@"ipAddress"] UTF8String]);
 		if (strcmp([[srv valueForKey:@"ipAddress"] UTF8String],  [ipAddress UTF8String]) == 0) {
 			NSLog(@"Found.");
-			return srv;
+			[servers release];
+			return [srv retain];
 		}
 	}
+	[servers release];
 	NSLog(@"Server not found.");
 	return nil;
 }
@@ -66,10 +153,6 @@
 - (void)setConnection:(ThriftConnection *)connection 
 			forServer:(NSManagedObject*)server
 {
-	if (!connectionsCache) {
-		NSLog(@"Initializing connections cache");
-		connectionsCache = [[NSMutableDictionary alloc] init];
-	}
 	id ipAddress = [server valueForKey:@"ipAddress"];
 	[connectionsCache setObject:connection forKey:ipAddress];
 	NSLog(@"Updaing cache with connection \"%s:%d\" for server \"%s\"", [connection.connInfo.address UTF8String], 
