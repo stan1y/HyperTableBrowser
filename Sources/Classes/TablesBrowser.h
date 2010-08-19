53471090267ef1fe4abc967ced7006a2655b6d68
Stanislav Yudin <Stanly@Silverblaze.local>
2010-08-19 18:36:52 +0530

    move toolbar callbacks to browser obj, implement NSBrowser callbacks

--- Sources/Classes/TablesBrowser.h
+++ Sources/Classes/TablesBrowser.h
@@ -11,19 +11,56 @@
 #import <InsertNewRowController.h>
 #import <HyperTableOperationController.h>
 #import <TablesBrowserPageSource.h>
-#import <TablesBrowserToolbar.h>
 
 @interface TablesBrowser : HyperTableOperationController {
+	
+	//panel controls
 	NewTableController * newTableController;
 	InsertNewRowController * newRowController;
-	TablesBrowserToolbarController * toolbarController;
 	
 	NSPanel * newTablePnl;
 	NSPanel * insertNewRowPnl;
+	NSBrowser * tablesList;
 	
 	TablesBrowserPageSource * pageSource;
+	
+	//toolbar controls
+	NSToolbarItem * newTableBtn;
+	NSToolbarItem * dropTableBtn;
+	NSToolbarItem * refreshBtn;
+	NSToolbarItem * newRowBtn;
+	NSToolbarItem * dropRowBtn;
+	NSToolbar * toolBar;
+	
+	int allowNewTable;
+	int allowDropTable;
+	int allowRefresh;
+	int allowInsertRow;
+	int allowDeleteRow;
+	
+	//selections
+	NSString * selectedTable;
 }
-@property (nonatomic, retain) IBOutlet TablesBrowserToolbarController * toolbarController;
+//selection properties
+@property (nonatomic, readonly, retain) NSString * selectedTable;
+
+//toolbar properties 
+@property (nonatomic, retain) IBOutlet NSBrowser * tablesList;
+@property (nonatomic, retain) IBOutlet NSToolbarItem * refreshBtn;
+@property (nonatomic, retain) IBOutlet NSToolbarItem * newTableBtn;
+@property (nonatomic, retain) IBOutlet NSToolbarItem * dropTableBtn;
+@property (nonatomic, retain) IBOutlet NSToolbarItem * newRowBtn;
+@property (nonatomic, retain) IBOutlet NSToolbarItem * dropRowBtn;
+
+@property (nonatomic, retain) IBOutlet NSToolbar * toolBar;
+
+@property (assign) int allowRefresh;
+@property (assign) int allowNewTable;
+@property (assign) int allowDropTable;
+@property (assign) int allowInsertRow;
+@property (assign) int allowDeleteRow;
+
+//panel properties
 @property (nonatomic, retain) IBOutlet TablesBrowserPageSource * pageSource;
 
 @property (nonatomic, retain) IBOutlet NSPanel * newTablePnl;
@@ -31,5 +68,26 @@
 
 @property (nonatomic, retain) IBOutlet NewTableController * newTableController;
 @property (nonatomic, retain) IBOutlet InsertNewRowController * newRowController;
+
+- (IBAction)newTable:(id)sender;
+- (IBAction)dropTable:(id)sender;
+- (IBAction)refreshTables:(id)sender;
+
+- (IBAction)insertNewRow:(id)sender;
+- (IBAction)deleteSelectedRow:(id)sender;
+
+//tool bar validation
+- (BOOL)validateToolbarItem:(NSToolbarItem *)toolbarItem;
+
+//tablesList delegate's protocol
+- (BOOL)browser:(NSBrowser *)browser shouldEditItem:(id)item;
+- (BOOL)browser:(NSBrowser *)sender isColumnValid:(NSInteger)column;
+- (BOOL)browser:(NSBrowser *)browser isLeafItem:(id)item; //yes means non-expandable
+
+- (id)rootItemForBrowser:(NSBrowser *)browser;
+- (id)browser:(NSBrowser *)browser objectValueForItem:(id)item;
+- (id)browser:(NSBrowser *)browser child:(NSInteger)index ofItem:(id)item;
+
+- (NSInteger)browser:(NSBrowser *)browser numberOfChildrenOfItem:(id)item;
 
 @end
--- Sources/Classes/TablesBrowser.m
+++ Sources/Classes/TablesBrowser.m
@@ -7,12 +7,14 @@
 //
 
 #import "TablesBrowser.h"
-
+#import <DeleteRowOperation.h>
+#import <HyperTable.h>
 
 @implementation TablesBrowser
 
 @synthesize pageSource;
-@synthesize toolbarController;
+@synthesize selectedTable;
+@synthesize tablesList;
 
 @synthesize newTablePnl;
 @synthesize insertNewRowPnl;
@@ -20,14 +22,53 @@
 @synthesize newTableController;
 @synthesize newRowController;
 
+@synthesize newTableBtn;
+@synthesize dropTableBtn;
+@synthesize refreshBtn;
+@synthesize newRowBtn;
+@synthesize dropRowBtn;
+
+@synthesize toolBar;
+
+@synthesize allowNewTable;
+@synthesize allowDropTable;
+@synthesize allowRefresh;
+@synthesize allowInsertRow;
+@synthesize allowDeleteRow;
+
+- (BOOL)validateToolbarItem:(NSToolbarItem *)toolbarItem
+{
+    if ([toolbarItem isEqual:newTableBtn]) {
+		return allowNewTable;
+    } else if ( [toolbarItem isEqual:dropTableBtn]) {
+		return allowDropTable;
+    } else if ( [toolbarItem isEqual:refreshBtn]) {
+		return allowRefresh;
+	} else if ( [toolbarItem isEqual:newRowBtn]) {
+		return allowInsertRow;
+	} else if ( [toolbarItem isEqual:dropRowBtn]) {
+		return allowDeleteRow;
+	}
+	
+	return YES;
+}
+
 - (void) dealloc
 {
-	[toolbarController release];
+	[selectedTable release];
 	[pageSource release];
 	[newTablePnl release];
 	[newTableController release];
 	[insertNewRowPnl release];
 	[newRowController release];
+	
+	[refreshBtn release];
+	[newTableBtn release];
+	[dropTableBtn release];
+	[newRowBtn release];
+	[dropRowBtn release];
+	[toolBar release];
+	
 	[super dealloc];
 }
 
@@ -35,5 +76,169 @@
 {
 	NSLog(@"Tables Browser closed\n");
 }
+
+- (IBAction)newTable:(id)sender
+{
+	[NSApp beginSheet:[[[NSApp delegate] tablesBrowser] newTablePnl] 
+	   modalForWindow:[[[NSApp delegate] tablesBrowser] window]
+        modalDelegate:self didEndSelector:nil contextInfo:nil];
+}
+
+- (IBAction)dropTable:(id)sender
+{
+	id connection = [self getSelectedConnection];
+	if (!connection) {
+		[self setMessage:@"Cannot drop table. Server is NOT connected."];
+		return;
+	}
+	
+	[self indicateBusy];
+	int rc = drop_table([connection thriftClient], [selectedTable UTF8String]);
+	
+	if (rc != T_OK) {
+		[self setMessage:[NSString stringWithFormat:@"Failed to drop table \"%s\". %s",
+													  [selectedTable UTF8String],
+													  [[HyperTable errorFromCode:rc] UTF8String]]];
+		[self indicateDone];
+	}
+	else {
+		NSString * msg = [NSString stringWithFormat:@"Table \"%s\" was dropped.",
+						  [selectedTable UTF8String]];
+		
+		//refresh tables on connection
+		FetchTablesOperation * fetchTablesOp = [FetchTablesOperation fetchTablesFromConnection:connection];
+		[fetchTablesOp setCompletionBlock: ^ {
+			NSLog(@"Refreshing tables on \"%s\"\n", [[[connection connInfo] address] UTF8String] );
+			
+			//FIXME: servers view
+			//[[[NSApp delegate] serversView] reloadItem:nil reloadChildren:YES];
+			//[[[NSApp delegate] serversView] deselectAll:self];
+			
+			[self setMessage:msg];
+			[self indicateDone];
+		}];
+		
+		//start fetching tables
+		[[[NSApp delegate] operations] addOperation: fetchTablesOp];
+		[fetchTablesOp release];
+	}
+	
+	[selectedTable release];
+}
+
+- (IBAction)refreshTables:(id)sender
+{
+	id hypertable = [self getSelectedConnection];
+	if (!hypertable) {
+		[self setMessage:@"No connection is available"];
+		return;
+	}
+	[self setMessage:@"Reloading tables list items..."];
+	[self indicateBusy];
+	[hypertable refresh:^ {
+		[self setMessage:@"Tables updated sucessfuly."];
+		[tablesList loadColumnZero];
+		[self indicateDone];
+	}];
+}
+
+- (IBAction)insertNewRow:(id)sender
+{
+	id connection = [self getSelectedConnection];
+	[[self newRowController] setConnection:connection];
+	[NSApp beginSheet:[self insertNewRowPnl] modalForWindow:[self window]
+        modalDelegate:self didEndSelector:nil contextInfo:nil];
+	[connection release];
+}
+
+- (IBAction)deleteSelectedRow:(id)sender
+{
+	NSString * selectedRowKeyValue = [[self pageSource] selectedRowKeyValue];
+	if (!selectedRowKeyValue || [selectedRowKeyValue length] <= 0 ) {
+		NSLog(@"No row selected for delete");
+		return;
+	}
+	
+	NSString * selectedTable = [[self tablesList] selectedTable];
+	if (!selectedTable) {
+		NSLog(@"No table is selected to insert row");
+		return;
+	}
+	
+	NSLog([NSString stringWithFormat:@"Deleteing row with key \"%s\" from table \"%s\".", 
+		   [[self selectedRowKeyValue] UTF8String],
+		   [selectedTable UTF8String]]);
+	
+	id connection = [self getSelectedConnection];
+	if (!connection) {
+		[self setMessage:@"Cannot delete selected row. Server is NOT connected."];
+		return;
+	}
+	
+	//drop row
+	DataPage * currentPage = [[self pageSource] page];
+	int selectedRowIndex = [[self pageSource] selectedRowIndex];
+	DataRow * selectedRow = page_row_at_index(currentPage, selectedRowIndex);
+	DeleteRowOperation * delOp = [DeleteRowOperation deleteRow:selectedRow
+													   inTable:selectedTable
+												withConnection:connection];
+	
+	[[[NSApp delegate] operations] addOperation:delOp];
+	[selectedRowKeyValue release];
+	[delOp release];
+	[connection release];
+}
+
+- (BOOL)browser:(NSBrowser *)browser shouldEditItem:(id)item
+{ 
+	//edit not supported yet
+	return NO; 
+}
+
+- (BOOL)browser:(NSBrowser *)sender isColumnValid:(NSInteger)column
+{
+	return [[[self getSelectedConnection] tables] count] > 0;
+}
+
+//yes means non-expandable
+- (BOOL)browser:(NSBrowser *)browser isLeafItem:(id)item 
+{
+	if ([item class] == [HyperTable class]) {
+		return NO;
+	}
+	return YES;
+}
+
+- (id)rootItemForBrowser:(NSBrowser *)browser
+{
+	return [self getSelectedConnection];
+}
+
+- (id)browser:(NSBrowser *)browser objectValueForItem:(id)item
+{
+	if ([item class] == [HyperTable class])
+		return [item ipAddress];
+	else {
+		return item;
+	}
+
+}
+- (id)browser:(NSBrowser *)browser child:(NSInteger)index ofItem:(id)item
+{
+	if ([item class] == [HyperTable class])
+	{
+		id table = [[[self getSelectedConnection] tables] objectAtIndex:index];
+		return table;
+	}
+}
+
+- (NSInteger)browser:(NSBrowser *)browser numberOfChildrenOfItem:(id)item
+{
+	if ([item class] == [HyperTable class]) {
+		return [[[self getSelectedConnection] tables] count];
+	}
+	return 0;
+}
+
 
 @end
