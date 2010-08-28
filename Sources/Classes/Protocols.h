92b5e6cbf39bde5eba8aad73b4baeebd86457940
Stanislav Yudin <Stanly@Silverblaze.local>
2010-08-28 19:54:28 +0530

    - add new protocol definitions - remove old ones - remove global cluster update operation

---
+++ Sources/Classes/Protocols.h
@@ -0,0 +1,35 @@
+//
+//  Protocols.h
+//  HyperTableBrowser
+//
+//  Created by Stanislav Yudin on 27/8/2010.
+//  Copyright 2010 AwesomeStanly Lab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "SSHClient.h"
+
+#define DATA_PAGE void*
+
+@protocol ClusterMember
+
+- (void) updateStatusWithCompletionBlock:(void (^)(BOOL))codeBlock;
+- (NSArray *) services;
+- (Service *) serviceWithName:(NSString *)name;
+
+@end
+
+@protocol CellStorage
+
+- (void) updateTablesWithCompletionBlock:(void (^)(BOOL))codeBlock;
+- (NSArray *) tables;
+
+- (void)fetchPageFrom:(id)tableID number:(int)number ofSize:(int)size 
+  withCompletionBlock:(void (^)(DATA_PAGE))codeBlock;
+
+- (int) lastFetchedIndex;
+- (int) lastFetchedTotalIndexes;
+
+- (BOOL) isConnected;
+- (void) connectedWithCompletionBlock:(void (^)(BOOL))codeBlock;
+@end
